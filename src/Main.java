e104f8a08fec088e6de4c3d8b5d410dc99645a4b
ruslanbek95 <kg_tulit@inbox.ru>
2023-03-24 22:07:52 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+public class Main {
+    public static void main(String[] args) {
+        //int a = 10;
+        // int w = 10;
+        // int d = 10;
+        //metod1(12,32);
+        // System.out.println(metod3(1, 2,4, 5, 6 ,12,3,43,545)) ;
+        // metod(a);
+        // int[ ] a = new int[]{1,24,5,65,7,88};
+//       stringtayp();
+
+
+        String name = " My name is Ruslanbe";
+
+       System.out.println(name.trim());
+
+      int a = 12;
+      int s = 34;
+       if ( a == s){
+           System.out.println( "barar emes ");
+
+           }else{
+               System.out.println(" barabar");
+           }
+
+
+
+    }
+    //  static void metod(int a){
+    //   System.out.println(a);
+    // }
+    // static void metod1(int w, int d ){
+    //     System.out.println(w+d );
+
+    //    static int metod3(int... a) {
+//        int summa = 0;
+//        for (int i = 0; i < a.length; i++) {
+//            summa = summa + a [i];
+//            }
+//
+//        return summa;
+//    static void stringtayp( ){
+//        String [] at = new String[]{"Ruslan " ,"Bektur", " Ackat"};
+//        for (String s : at) {
+//            System.out.println("мен сени жакшы кором  "  + s); String name = "My name is Ruslan";
+    {
+        String name = " My name is Ruslanbe";
+        System.out.println(name.substring(4,7));
+    }
+}
+
+
+
+
+
+
+
